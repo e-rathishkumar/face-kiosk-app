5001c6f81d97f5dd223af0df458d0660af46a14e
Rathishkumar E <>
2026-06-16 18:20:08 +0530

    Add defaults to pydantic settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,10 +9,10 @@ class Settings(BaseSettings):
     REDIS_URL: str
 
     JWT_SECRET: str
-    JWT_ALGORITHM: str
+    JWT_ALGORITHM: str = "HS256"
 
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    REFRESH_TOKEN_EXPIRE_DAYS: int
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     model_config = SettingsConfigDict(
         env_file=".env",
